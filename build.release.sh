7f763c48f5670e5cc658b61bb7aa6c4d6b97e82a
Artem Varaksa <artvaraksa@gmail.com>
2021-10-16 20:33:03 +0300

    Use prebuilt `trunk` for releases.

--- build.release.sh
+++ build.release.sh
@@ -3,6 +3,6 @@ set -e
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 rustup target add wasm32-unknown-unknown
-cargo install trunk
+wget -qO- https://github.com/thedodd/trunk/releases/download/v0.14.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 yarn install
-trunk build --release
+./trunk build --release
