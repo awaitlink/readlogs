8df89b9fce987a732743c5bf25aa38a0f2b3a3fe
Artem Varaksa <artvaraksa@gmail.com>
2021-10-29 14:00:06 +0300

    Parse new NSE name.
    Also use `nom` for `AppId` parsing to avoid panics.

--- src/parsers/ios_filename.rs
+++ src/parsers/ios_filename.rs
@@ -1,25 +1,22 @@
 use chrono::prelude::*;
 use derive_more::Display;
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_until},
     character::complete::space1,
-    combinator::rest,
+    combinator::{rest, value},
     sequence::{delimited, preceded, terminated},
     IResult,
 };
-use strum_macros::EnumString;
 
 use crate::{impl_from_str, parsers::common};
 
-#[derive(Debug, Display, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, EnumString)]
+#[derive(Debug, Display, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum AppId {
-    #[strum(serialize = "org.whispersystems.signal")]
     Signal,
     #[display(fmt = "NSE")]
-    #[strum(serialize = "org.whispersystems.signal.NotificationServiceExtension")]
     NotificationServiceExtension,
     #[display(fmt = "SAE")]
-    #[strum(serialize = "org.whispersystems.signal.shareextension")]
     ShareAppExtension,
 }
 
@@ -34,11 +31,31 @@ pub struct LogFilename {
     pub extension: String,
 }
 
+fn app_id_with_space(input: &str) -> IResult<&str, AppId> {
+    preceded(
+        tag("org.whispersystems.signal"),
+        alt((
+            value(AppId::Signal, space1),
+            terminated(
+                alt((
+                    value(AppId::NotificationServiceExtension, tag(".SignalNSE")),
+                    value(
+                        AppId::NotificationServiceExtension,
+                        tag(".NotificationServiceExtension"),
+                    ),
+                    value(AppId::ShareAppExtension, tag(".shareextension")),
+                )),
+                space1,
+            ),
+        )),
+    )(input)
+}
+
 fn log_filename(input: &str) -> IResult<&str, LogFilename> {
     let (remainder, submission_time) =
         common::naive_date_time(None, ".", " ", ".", None, None)(input)?;
     let (remainder, folder_id) = delimited(tag(" "), take_until("/"), tag("/"))(remainder)?;
-    let (remainder, app_id) = terminated(take_until(" "), space1)(remainder)?;
+    let (remainder, app_id) = app_id_with_space(remainder)?;
     let (remainder, file_time) =
         common::naive_date_time(None, "-", "--", "-", Some("-"), None)(remainder)?;
     let (remainder, extension) = preceded(tag("."), rest)(remainder)?;
@@ -48,7 +65,7 @@ fn log_filename(input: &str) -> IResult<&str, LogFilename> {
         LogFilename {
             submission_time,
             folder_id: folder_id.to_owned(),
-            app_id: app_id.parse().unwrap(),
+            app_id,
             file_time: DateTime::<Utc>::from_utc(file_time, Utc),
             extension: extension.to_owned(),
         },
@@ -59,7 +76,11 @@ impl_from_str!(log_filename => LogFilename);
 
 #[cfg(test)]
 mod tests {
+    use test_case::test_case;
+
     use super::*;
+
+    use crate::utils::parsing_test;
 
     #[test]
     fn partial_ord() {
@@ -90,8 +111,16 @@ mod tests {
         assert!(c < b);
     }
 
+    #[test_case("org.whispersystems.signal " => AppId::Signal)]
+    #[test_case("org.whispersystems.signal.SignalNSE " => AppId::NotificationServiceExtension)]
+    #[test_case("org.whispersystems.signal.NotificationServiceExtension " => AppId::NotificationServiceExtension)]
+    #[test_case("org.whispersystems.signal.shareextension " => AppId::ShareAppExtension)]
+    fn app_id_with_space_ok(input: &str) -> AppId {
+        parsing_test(app_id_with_space, input)
+    }
+
     #[test]
-    fn ok() {
+    fn log_filename_ok() {
         let (remainder, result) = log_filename("1234.01.23 12.34.56 ABCD1234-1AB2-3CDE-456F-789AB0CD1E2F/org.whispersystems.signal 1234-01-22--06-54-32-109.log").unwrap();
 
         assert_eq!(remainder, "", "remainder should be empty");
