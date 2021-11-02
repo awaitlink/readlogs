8fd260a660eaa971df232f17ef5d10299f38f81a
Artem Varaksa <artvaraksa@gmail.com>
2021-11-02 14:30:37 +0300

    Use nightly Rust.

--- build.release.sh
+++ build.release.sh
@@ -1,8 +1,9 @@
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain nightly-wasm32-unknown-unknown -y
 source $HOME/.cargo/env
-rustup target add wasm32-unknown-unknown
+rustup show
+rustc -vV
 cargo install trunk
 yarn install
 trunk build --release
