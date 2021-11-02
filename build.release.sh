fe1ec33dcca52a5306c5e07741a2603ab7f9bdd6
Artem Varaksa <artvaraksa@gmail.com>
2021-11-02 14:37:37 +0300

    Fix target in build script.

--- build.release.sh
+++ build.release.sh
@@ -1,7 +1,8 @@
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain nightly-wasm32-unknown-unknown -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain nightly -y
 source $HOME/.cargo/env
+rustup target add wasm32-unknown-unknown
 rustup show
 rustc -vV
 cargo install trunk
